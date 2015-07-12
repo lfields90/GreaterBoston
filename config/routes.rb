d26dd27a556eb9ac04a8fbefad0566d13dc4998c
Twelvvy <lamonte.fields@gmail.com>
2015-07-11 21:03:49 -0400

    Tweaked routes to hopefully add OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/auth/:twitter/callback', to: 'sessions#create'
+  get '/auth/:facebook/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
