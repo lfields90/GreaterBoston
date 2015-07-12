21368ccaea1fe6de44872c1618b23279cace56b3
Twelvvy <lamonte.fields@gmail.com>
2015-07-12 03:59:00 -0400

    Trying to fix tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+
   def self.from_omniauth(auth_hash)
       user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
       user.name = auth_hash['info']['name']
