d5839da30d9145eab39dd60bb2fe9b87ac506bf3
Twelvvy <lamonte.fields@gmail.com>
2015-07-13 17:19:43 -0400

    Schema Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713205043) do
+ActiveRecord::Schema.define(version: 20150713210241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.integer  "event_id",   null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "business_categories", force: :cascade do |t|
     t.string   "name",       null: false
