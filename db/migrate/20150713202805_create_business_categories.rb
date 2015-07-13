03db62b73b80c25862c143ff16d5fc74213f898a
Twelvvy <lamonte.fields@gmail.com>
2015-07-13 16:34:18 -0400

    Add Join Table Migration for BusCat

--- db/migrate/20150713202805_create_business_categories.rb
+++ db/migrate/20150713202805_create_business_categories.rb
@@ -1,6 +1,10 @@
 class CreateBusinessCategories < ActiveRecord::Migration
   def change
     create_table :business_categories do |t|
+      t.integer :business_id
+      t.integer :category_id
+
+      t.timestamps null: false
     end
   end
 end
