8eddbc1d454119fdaa869285bd2ce36c0867a0de
Twelvvy <lamonte.fields@gmail.com>
2015-07-13 16:34:48 -0400

    Add Join Table Migration for BusFeat

--- db/migrate/20150713202847_create_business_features.rb
+++ db/migrate/20150713202847_create_business_features.rb
@@ -1,6 +1,10 @@
 class CreateBusinessFeatures < ActiveRecord::Migration
   def change
     create_table :business_features do |t|
+      t.integer :business_id
+      t.integer :feature_id
+
+      t.timestamps null: false
     end
   end
 end
