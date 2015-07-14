a638b752feb303f807774f6b8143a5710beaf2ae
Twelvvy <lamonte.fields@gmail.com>
2015-07-14 12:49:41 -0400

    Add model test for city

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe City do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:state_id) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:state) }
+end
