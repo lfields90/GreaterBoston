03335cdd6a934f42518e7d9eb6c2ce2683e8ca3b
Twelvvy <lamonte.fields@gmail.com>
2015-07-14 10:52:34 -0400

    Removed Unecessary Migration

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,4 @@ ActiveRecord::Schema.define(version: 20150710062859) do
     t.string   "url"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
-  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
-
 end
