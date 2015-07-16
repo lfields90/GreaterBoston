4df165c5aeb8d56bd08c13fde48c05e56ee5316f
Twelvvy <lamonte.fields@gmail.com>
2015-07-16 14:03:22 -0400

    Add CarrierWave

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
 
+  mount_uploader :profile_photo, ProfilePhotoUploader
+
   def self.from_omniauth(auth_hash)
       user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
       user.name = auth_hash['info']['name']
