10da384dd0c14cff411cf4f636c735435f56adc6
Twelvvy <lamonte.fields@gmail.com>
2015-07-19 14:14:03 -0400

    Updated Category Test

--- spec/features/user_creates_a_category_spec.rb
+++ spec/features/user_creates_a_category_spec.rb
@@ -11,14 +11,12 @@ feature "User creates a category" do
 
     fill_in "Name", with: "Block Party"
     click_button "Add new category"
-
     expect(page).to have_content("Block Party")
   end
 
   scenario 'Try to create category while missing info' do
 
     click_button "Add new category"
-
     expect(page).to have_content("Name can't be blank")
   end
 end
